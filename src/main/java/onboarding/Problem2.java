f047b9c785c69e8fbda10610745f6f41f825258f
yoonseo choi <choiyoonseo02@gmail.com>
2023-12-05 15:39:42 +0900

    feat: problem2 solved

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,35 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+
+        List<String> splitCryptogram = new ArrayList<>(List.of(cryptogram.split("")));
+
+        int size;
+
+        do {
+            size = splitCryptogram.size();
+            int i = 1;
+
+            while (i < splitCryptogram.size()) {
+                if (Objects.equals(splitCryptogram.get(i), splitCryptogram.get(i - 1))) {
+                    splitCryptogram.remove(i);
+                    splitCryptogram.remove(i - 1);
+                } else {
+                    i++;
+                }
+            }
+        } while (size != splitCryptogram.size());
+
+        for (String s : splitCryptogram) {
+            answer.append(s);
+        }
+
+        return answer.toString();
     }
 }

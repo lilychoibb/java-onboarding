64802b2e83310d0d21d8501dd72091b3f1a82a3a
yoonseo choi <choiyoonseo02@gmail.com>
2023-12-05 16:37:59 +0900

    feat: problem4 solved

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,36 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        Map<Character, Character> map = createLetterMapping();
+
+        for (char c : word.toCharArray()) {
+            if (map.containsKey(c)) {
+                answer.append(map.get(c));
+            } else {
+                answer.append(c);
+            }
+        }
+
+        return answer.toString();
+    }
+
+    private static Map<Character, Character> createLetterMapping() {
+        Map<Character, Character> map = new HashMap<>();
+
+        char startChar = 'A';
+        char endChar = 'Z';
+
+        for (char currentChar = startChar; currentChar <= endChar; currentChar++) {
+            char mirroredChar = (char) ('Z' - (currentChar - 'A'));
+            map.put(currentChar, mirroredChar);
+            map.put(Character.toLowerCase(currentChar), Character.toLowerCase(mirroredChar));
+        }
+
+        return map;
     }
 }

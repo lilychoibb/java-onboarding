51083ba100fa8a10499868f4c66ee2ee03e52928
yoonseo choi <choiyoonseo02@gmail.com>
2023-12-05 16:02:25 +0900

    feat: problem3 solved

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,21 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        int[] gameNumber = new int[]{3, 6, 9};
+
+        for (int i = 0; i < number + 1; i++) {
+            String[] num = String.valueOf(i).split("");
+            for (int gameNum : gameNumber) {
+                for (String numb : num) {
+                    if (gameNum == Integer.parseInt(numb)) {
+                        answer += 1;
+                    }
+                }
+
+            }
+        }
+
         return answer;
     }
 }
